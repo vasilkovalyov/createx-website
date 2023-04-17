2a50c68a0ab97022faf3f1cf755b564aa60818f9
vasilkovalyov <vasilkovalyov@gmail.com>
2023-04-17 19:03:56 +0300

    refactored classname in client quote component

--- client/src/components/ClientQuote/index.tsx
+++ client/src/components/ClientQuote/index.tsx
@@ -9,7 +9,7 @@ function ClientQuote({ image, name, position, text, className }: IClientQuote) {
     <div className={cn('client-quote', className)}>
       {image ? <Image {...image} width={72} height={72} className="client-quote__image mb-24" /> : null}
       <Typography className="text-gray-800 text-md mb-24">{text}</Typography>
-      <Typography className="mb-8 font-bold text-md mb-4">{name}</Typography>
+      <Typography className="font-bold text-md mb-4">{name}</Typography>
       <Typography className="mb-8 text-gray-700 text-sm">{position}</Typography>
     </div>
   )
