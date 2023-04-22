e962d4a0d60a6342884359a09dbe3912119a1e8a
vasilkovalyov <vasilkovalyov@gmail.com>
2023-04-22 14:46:47 +0300

    strapi: created service inner page model

---
+++ backend/src/api/page-service/routes/page-service.ts
@@ -0,0 +1,7 @@
+/**
+ * page-service router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::page-service.page-service');
---
+++ backend/src/api/page-service/services/page-service.ts
@@ -0,0 +1,7 @@
+/**
+ * page-service service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::page-service.page-service');
